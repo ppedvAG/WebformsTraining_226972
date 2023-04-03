b097e3b757de5e39c8513d99ad100c2873a2a7cb
Hannes Preishuber <hannesp@ppedv.de>
2023-04-03 15:53:09 +0200

    LINQ Beispiele

---
+++ WebformsTraining_226972/LINQBeispiele.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="LINQBeispiele.aspx.vb" Inherits="WebformsTraining_226972.LINQBeispiele" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ WebformsTraining_226972/LINQBeispiele.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.Ajax.Utilities
+
+Public Class LINQBeispiele
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim db = New NorthwindContext
+
+        Dim kunden = db.Customers.Where(Function(x) x.Orders.Count > 3 AndAlso x.CompanyName.Contains("tt")).OrderBy(Function(x) x.City).ToList()
+        Dim kunden2 = kunden.Take(2)
+
+
+    End Sub
+
+End Class
