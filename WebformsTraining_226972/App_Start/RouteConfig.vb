616801b332618e478c4776fd96c769286401594f
Hannes Preishuber <hannesp@ppedv.de>
2023-04-05 10:17:17 +0200

    SignalR Versuch

--- WebformsTraining_226972/App_Start/RouteConfig.vb
+++ WebformsTraining_226972/App_Start/RouteConfig.vb
@@ -1,11 +1,12 @@
 ﻿Imports System.Web.Routing
 Imports Microsoft.AspNet.FriendlyUrls
-
+Imports Microsoft.AspNet.SignalR
 Public Module RouteConfig
     Sub RegisterRoutes(ByVal routes As RouteCollection)
         Dim settings As FriendlyUrlSettings = New FriendlyUrlSettings() With {
             .AutoRedirectMode = RedirectMode.Permanent
         }
         routes.EnableFriendlyUrls(settings)
+
     End Sub
 End Module
